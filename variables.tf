8b5e5b0257b4997f21c88b4525aa2e26ada3b70c
James Grow <jmgrow@gmail.com>
2022-04-05 02:23:52 -0500

    Updated default AWS region variable

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "AWS region for all resources."
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 variable "handler_name" {
